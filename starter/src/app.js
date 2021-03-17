f1dcfd906357733f1b1d75961a142c2d0e01a10c
Zac <ztaylor555@gmail.com>
2021-03-17 13:57:03 -0400

    init

--- starter/src/app.js
+++ starter/src/app.js
@@ -74,114 +74,47 @@ const mapStyle = [{
   ],
 },
 ];
-
 function initMap() {
+
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    center: {lat: 52.632469, lng: -1.689423},
+    center: {lat: 35.213959, lng: -80.018372},
     styles: mapStyle
   });
 
-  // Load the store markers into the map object as geoJSON data.
+   
+  // Load the stores GeoJSON onto the map.
   map.data.loadGeoJson('src/stores.json', {idPropertyName: 'storeid'});
-  console.log(map.data);
-  
-  //Add another reference to the API key and initialize the infowindow.
-  const apiKey = 'AIzaSyBamgndc8EvC6j1t8o-lpXCxJ1M9XPwyGg';
+
+    // Define the custom marker icons, using the store's "category".
+    // map.data.setStyle((feature) => {
+    //   return {
+    //     icon: {
+    //       url: `img/icon_${feature.getProperty('category')}.png`,
+    //       scaledSize: new google.maps.Size(64, 64),
+    //     },
+    //   };
+    // });
+
+  const apiKey = 'YOUR_API_KEY';
   const infoWindow = new google.maps.InfoWindow();
 
   // Show the information for a store when its marker is clicked.
   map.data.addListener('click', (event) => {
-    const category = event.feature.getProperty('category');
-    console.log(category);
     const name = event.feature.getProperty('name');
-    const description = event.feature.getProperty('description');
-    const hours = event.feature.getProperty('hours');
     const phone = event.feature.getProperty('phone');
+    const scheduleURL = event.feature.getProperty('schedulepage');
+    const clinicURL = event.feature.getProperty('clinicpage');
     const position = event.feature.getGeometry().get();
     const content = `
-    <img style="float:left; width:200px; margin-top:30px" src="./src/img/icon_cafe.png">
-    <div style="margin-left:220px; margin-bottom:20px;">
-      <h2>${name}</h2><p>${description}</p>
-      <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
-      <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}"></p>
-    </div>
+      <h2>${name}</h2><p></p>
+      <p><b>Phone:</b> ${phone}</p>
     `;
 
     infoWindow.setContent(content);
     infoWindow.setPosition(position);
     infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
     infoWindow.open(map);
-  });
-
-  // Build and add the search bar
-  const card = document.createElement('div');
-  const titleBar = document.createElement('div');
-  const title = document.createElement('div');
-  const container = document.createElement('div');
-  const input = document.createElement('input');
-
-  //Searchbox options to restrict the queries. In this case, by addresses in great britain.
-  const options = {
-    types: ['address'],
-    componentRestrictions: {country: 'gb'},
-  };
-
-    card.setAttribute('id', 'pac-card');
-    title.setAttribute('id', 'title');
-    title.textContent = 'Find the nearest store';
-    titleBar.appendChild(title);
-    container.setAttribute('id', 'pac-container');
-    input.setAttribute('id', 'pac-input');
-    input.setAttribute('type', 'text');
-    input.setAttribute('placeholder', 'Enter an address');
-    container.appendChild(input);
-    card.appendChild(titleBar);
-    card.appendChild(container);
-
-    //Pushes the div we create to the top right of the map.
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
-    // Make the search bar into a Places Autocomplete search bar and select
-    // which detail fields should be returned about the place that
-    // the user selects from the suggestions.
-    const autocomplete = new google.maps.places.Autocomplete(input, options);
-
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'name']);
-
-    // Set the origin point when the user selects an address
-    const originMarker = new google.maps.Marker({map: map});
-    originMarker.setVisible(true);
-    let originLocation = map.getCenter();
-
-    autocomplete.addListener('place_changed', async () => {
-    originMarker.setVisible(false);
-    originLocation = map.getCenter();
-    const place = autocomplete.getPlace();
-
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert('No address available for input: \'' + place.name + '\'');
-      return;
-    }
-
-    // Recenter the map to the selected address
-    originLocation = place.geometry.location;
-    map.setCenter(originLocation);
-    map.setZoom(9);
-    console.log(place);
-
-    originMarker.setPosition(originLocation);
-    originMarker.setVisible(true);
-
-    // Use the selected address as the origin to calculate distances
-    // to each of the store locations
-    const rankedStores = await calculateDistances(map.data, originLocation);
-    showStoresList(map.data, rankedStores);
-
-    return;
   });
 }
